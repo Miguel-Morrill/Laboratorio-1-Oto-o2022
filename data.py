5cef03ded39022cd6503e4ccd3aeb18b3cdcafcf
Miguel-Moreno-Morrill <thesolloyo@hotmail.com>
2022-09-08 23:28:47 -0500

    Modificación 2

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
-import data as dat
-import functions as fu
-import visualizations as vs
+import pandas as pd
+import numpy as np
+
+data1 = pd.read_csv("files/NAFTRAC_20200131.csv", skiprows=2)
+data2 = pd.read_csv("files/NAFTRAC_20200228.csv", skiprows=2)
+
+import glob
+data = pd.DataFrame()
+for f in glob.glob("files/*.csv"):
+    appenddata = pd.read_csv(f, header=None, sep=";", skiprows=2)
+    data = data.append(appenddata,ignore_index=True)
--- functions.py
+++
@@ -1,3 +0,0 @@
-import data as dat
-import functions as fu
-import visualizations as vs
